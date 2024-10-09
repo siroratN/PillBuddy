af0d9de657cbb59867ef27fb92ed28ea99e415f0
siroratN <siroratnbs@gmail.com>
2024-10-10 00:47:46 +0700

    clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-'use server'
-import { db } from '../../drizzle/db';
-import { users } from '../../drizzle/schema';
-import { UserSchema } from '../../drizzle/schema';
-
-export default async function HomePage() {
-  // Query ข้อมูลจากตาราง users
-  const allUsers = await db.select().from(users);
-
-  return (
-    <div>
-      <h1>Users List</h1>
-      <ul>
-        {allUsers.map((user:UserSchema) => (
-          <li key={user.id}>
-            {user.id} - {user.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+export default function Home() {
+    return (
+      <div>
+        <h1 className='font-bold text-center mt-10'>Home page</h1>
+      </div>
+    );
+  }
---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='flex items-center justify-center flex-col gap-10'>
+      <div className='mt-28'>
+      <SignIn />
+      </div>
+    </div>
+  );
+}
