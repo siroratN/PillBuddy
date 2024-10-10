839ecfa1b23bc3c9bed2ef9cb77b2b113153e8aa
siroratN <siroratnbs@gmail.com>
2024-10-10 23:03:28 +0700

    t

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,16 @@ export default function Page() {
   return (
     <div className='flex items-center justify-center flex-col gap-10'>
       <div className='mt-28'>
-      <SignIn />
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary: 'bg-green-600 hover:bg-green-400 text-white font-semibold py-2 px-4 rounded shadow-md border-none',
+              card: 'shadow-lg border border-gray-200 rounded-lg',
+              headerTitle: 'text-xl font-bold text-gray-800',
+              formFieldInput: 'border border-gray-300 p-2 rounded-md',
+            },
+          }}
+        />
       </div>
     </div>
   );
