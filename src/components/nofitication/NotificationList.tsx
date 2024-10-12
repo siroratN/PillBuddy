749457abbe6e42d3c4d6b7c863d24b0598405a77
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-10-12 19:10:45 +0700

    add router to each noti

--- src/components/nofitication/NotificationList.tsx
+++ src/components/nofitication/NotificationList.tsx
@@ -1,10 +1,14 @@
 'use client';
 import React from 'react';
 import { NotificationSchema } from '../../../drizzle/schema';
+import { useRouter } from 'next/navigation';
 
 const NotificationList = ({ notification }: { notification: NotificationSchema }) => {
+    const router = useRouter()
 	return (
-		<div key={notification.id} className="p-4 place-items-center bg-neutral-400 w-32">
+		<div key={notification.id} className="p-4 cursor-pointer place-items-center bg-neutral-400 w-32" onClick={()=>{
+            router.push(`/notification/${notification.id}`)
+        }}>
 			<p className="text-xl font-semibold">{notification.meal}</p>
 			<p className="text-gray-500">{notification.notification_status}</p>
 			<p className="text-gray-500">{notification.notification_time}</p>
