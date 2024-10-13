cab9a1c95398a29517735127f2aaf0cae9ad1179
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-10-14 01:23:55 +0700

    Update route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,33 +5,31 @@ import { users, patients, caregivers } from '../../../../drizzle/schema';
 import { eq } from 'drizzle-orm';
 
 export async function POST(req: NextRequest) {
-    try {
-        const { id, name } = await req.json(); 
+	try {
+		const { id, name } = await req.json();
 
-        console.log('Received:', id, name);
+		console.log('Received:', id, name);
 
-        const existingUser = await db
-            .select()
-            .from(users)
-            .where(eq(users.clerkID, id))
-            .execute();
+		const existingUser = await db.select().from(users).where(eq(users.clerkID, id)).execute();
 
-        if (existingUser.length > 0) {
-            return NextResponse.json({ message: 'User with this clerkID already exists' }, { status: 409 });
-        }
-        await db.insert(users).values({
-            clerkID: id,
-            name: name,
-        });
-        return NextResponse.redirect('/role'), { status: 201 };
-    } catch (error) {
-        console.error('Error saving user:', error);
-        return NextResponse.json({ message: 'Failed to save user' }, { status: 500 });
-    }
+		if (existingUser.length > 0) {
+			return NextResponse.json(
+				{ message: 'User with this clerkID already exists' },
+				{ status: 409 }
+			);
+		}
+		await db.insert(users).values({
+			clerkID: id,
+			name: name,
+		});
+		return NextResponse.redirect('/role'), { status: 201 };
+	} catch (error) {
+		console.error('Error saving user:', error);
+		return NextResponse.json({ message: 'Failed to save user' }, { status: 500 });
+	}
 }
 
-
 export async function GET(req: NextRequest) {
-    const { id } = await req.json();
-    console.log(id) 
+	const { id } = await req.json();
+	console.log(id);
 }
